94eacf56c83b43bd79baaacc03c60791654da134
Explode <54460101+senbosverbeni@users.noreply.github.com>
2022-05-23 23:26:11 +0300

    Add files via upload

---
+++ src/Configs/Achievements.js
@@ -0,0 +1,33 @@
+module.exports = {
+
+    "chat_achis": {
+    "achi1": {"name": "İlk Mesajım", "point": 1},
+    "achi2": {"name": "Mesaj Stajyeri I", "point": 200},
+    "achi3": {"name": "Mesaj Stajyeri II", "point": 500},
+    "achi4": {"name": "Mesaj Stajyeri III", "point": 800},
+    "achi5": {"name": "Mesaj Ustası I", "point": 1250},
+    "achi6": {"name": "Mesaj Ustası II", "point": 2000},
+    "achi7": {"name": "Mesaj Ustası III", "point": 5000},
+    "achi8": {"name": "Mesaj Kralı I", "point": 10000},
+    "achi9": {"name": "Mesaj Kralı II", "point": 15000},
+    "achi10": {"name": "Yenilmez Mesaj Ustası", "point": 27500},
+    "achi11": {"name": "Efsanevi Mesaj Ustası", "point": 35000},
+    "achi12": {"name": "Yenilmez/Yıkılmaz/Efsanevi Mesaj Ustası", "point": 50000}
+    },
+
+    "voice_achis": {
+        "achi1": {"name": "İlk Sohbet", "point": 1},
+        "achi2": {"name": "Sohbet Stajyeri I", "point": 100},
+        "achi3": {"name": "Sohbet Stajyeri II", "point": 180},
+        "achi4": {"name": "Sohbet Stajyeri III", "point": 400},
+        "achi5": {"name": "Sohbet Ustası I", "point": 900},
+        "achi6": {"name": "Sohbet Ustası II", "point": 1300},
+        "achi7": {"name": "Sohbet Ustası III", "point": 2100},
+        "achi8": {"name": "Sohbet Kralı I", "point": 3400},
+        "achi9": {"name": "Sohbet Kralı II", "point": 5000},
+        "achi10": {"name": "Yenilmez Mesaj Ustası", "point": 12000},
+        "achi11": {"name": "Efsanevi Sohbet Ustası", "point": 20000},
+        "achi12": {"name": "Yenilmez/Yıkılmaz/Efsanevi Sohbet Ustası", "point": 30000}
+        }
+
+}
---
+++ src/Events/appConnection.js
@@ -0,0 +1,9 @@
+const Discord = require("discord.js");
+
+module.exports = async () => {
+    client.user.setActivity("Created by Devils")
+   }; 
+
+   module.exports.config = {
+       name: "ready"
+     }
